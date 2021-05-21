a78738c90dfd6f5420d9f1c229573e67271d8696
Jiyong <o1067426676@gmail.com>
2021-05-21 22:55:23 +0900

    -최초 커밋-

---
+++ src/main/java/kr/ac/inha/board/controller/BoardController.java
@@ -0,0 +1,24 @@
+package kr.ac.inha.board.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BoardController {
+	@RequestMapping("/hello")
+	public String hello() {
+		return "hello world!!";
+	}
+	@RequestMapping("/inha")
+	public String inha(String param) {
+		return param + "inha.ac.kr";
+	}
+	@RequestMapping("/name")
+	public String name(String id) {
+		return "당신의 학번은 " + id + "입니다.";
+	}
+//	@RequestMapping("/gugu")
+//	public String gugu(int a) {
+//		
+//	}
+}
