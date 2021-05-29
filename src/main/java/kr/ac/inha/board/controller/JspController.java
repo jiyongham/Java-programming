a5f9b55d88f3ff61fe796326feea02d44bd590ec
Jiyong <o1067426676@gmail.com>
2021-05-29 20:41:48 +0900

    jsp

--- src/main/java/kr/ac/inha/board/controller/JspController.java
+++ src/main/java/kr/ac/inha/board/controller/JspController.java
@@ -1,8 +1,10 @@
 package kr.ac.inha.board.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+@Controller
 public class JspController {
 	@RequestMapping("/")
 	public ModelAndView hello() {
@@ -14,14 +16,22 @@ public class JspController {
 	public String helloDo() {
 		return "hello";
 	}
-	@RequestMapping("/gugudan")
-	public ModelAndView gugudan(int n) {
+//	@RequestMapping("/gugudan")
+//	public ModelAndView gugudan(int n) {
+//		ModelAndView mv = new ModelAndView();
+//		mv.setViewName("gugudan");
+//		String result = "구구단 : ";
+//		for(int i = 1; i<= 9; i++)
+//			result = result + n + "*" + i + "=" + (n*i) + "";
+//		mv.addObject("gugu", result);
+//		return mv;
+//	}
+	@RequestMapping("/sum")
+	public ModelAndView sum() {
 		ModelAndView mv = new ModelAndView();
 		mv.setViewName("gugudan");
-		String result = "구구단 : ";
-		for(int i = 1; i<= 9; i++)
-			result = result + n + "*" + i + "=" + (n*i) + "";
-		mv.addObject("gugu", result);
+		int sum = 5050;
+		mv.addObject("gugu", sum);
 		return mv;
 	}
 }
